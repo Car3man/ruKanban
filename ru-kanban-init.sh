28d5566372b92dc57c155ffd59fae2531704797f
name <email>
2023-01-13 00:16:35 +0300

    + Added utility scripts

---
+++ ru-kanban-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+REP_REMOTE_URL="git@github.com:Car3man/ruKanban.git"
+REP_LOCAL_PATH="./ruKanban"
+
+if [[ -d $REP_LOCAL_PATH ]]
+then
+	echo $REP_LOCAL_PATH "folder exists, please remove this dir before execute script"
+	exit
+fi
+
+git clone git@github.com:Car3man/ruKanban.git
