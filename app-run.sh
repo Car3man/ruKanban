c1934c8fbe5aca26e271a962c5c84c37fffd1214
name <email>
2023-01-13 14:38:36 +0300

    + added react-app

--- app-run.sh
+++ app-run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+cd ./react-app 
+npm run build
+cd ..
+rm -r -f ./web-server/usr/share/nginx/html/*
+cp -a ./react-app/build/* ./web-server/usr/share/nginx/html
 docker-compose up --build --remove-orphans
