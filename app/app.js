c6aaa669a3639978911b96b68659441d7b14e0da
name <email>
2023-01-13 03:08:44 +0300

    + Added base infrastructure of project

---
+++ app/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const port = 80
+const app = express()
+
+app.get('/', (request, response) => {
+    console.log(`URL: ${request.url}`)
+    response.send('Hello, Server!')
+});
+
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`)
+    console.log(`Server listening on port ${server.address().port}`)
+});
