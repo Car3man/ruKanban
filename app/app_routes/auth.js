8ba4db3eee1cd2a575cf546dc9c8d646a4727902
name <email>
2023-01-17 06:02:00 +0300

    ~ using middlewares in '/auth' endpoints

--- app/app_routes/auth.js
+++ app/app_routes/auth.js
@@ -1,13 +1,14 @@
 const { Router } = require("express")
 const { sendNotFound } = require("../common/response-helper")
+const { unauthorizedRequiredMiddleware, authenticateMiddleware } = require("../common/auth-helper")
 const { signUp, signIn, signOut, changePassword } = require("./auth-controller")
 
 const router = Router()
 
-router.post("/signUp", signUp)
-router.post("/signIn", signIn)
-router.post("/signOut", signOut)
-router.post("/changePassword", changePassword)
+router.post("/signUp", unauthorizedRequiredMiddleware, signUp)
+router.post("/signIn", unauthorizedRequiredMiddleware, signIn)
+router.post("/signOut", authenticateMiddleware, signOut)
+router.post("/changePassword", authenticateMiddleware, changePassword)
 router.all("*", sendNotFound)
 
 module.exports = router
