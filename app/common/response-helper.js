7c60f215c02228013dac9e38af3b9c099db64df2
name <email>
2023-01-17 05:08:21 +0300

    ~ support data overriding, new exports new exports for internalServerError alias

--- app/common/response-helper.js
+++ app/common/response-helper.js
@@ -4,18 +4,22 @@ module.exports.sendOk = (req, res, data) => {
     res.status(httpCodes.ok).send(data)
 }
 
-module.exports.sendBadRequest = (req, res) => {
-    res.status(httpCodes.badRequest).send("The request was unacceptable, often due to missing a required parameter.")
+module.exports.sendBadRequest = (req, res, data) => {
+    res.status(httpCodes.badRequest).send(data || "The request was unacceptable, often due to missing a required parameter.")
 }
 
-module.exports.sendUnauthorized = (req, res) => {
-    res.status(httpCodes.unauthorized).send("The authorization required.")
+module.exports.sendUnauthorized = (req, res, data) => {
+    res.status(httpCodes.unauthorized).send(data || "The authorization required.")
 }
 
-module.exports.sendForbidden = (req, res) => {
-    res.status(httpCodes.unauthorized).send("The user doesn't have permissions to perform the request.")
+module.exports.sendForbidden = (req, res, data) => {
+    res.status(httpCodes.unauthorized).send(data || "The user doesn't have permissions to perform the request.")
 }
 
-module.exports.sendNotFound = (req, res) => {
-    res.status(httpCodes.notFound).send("The requested resource doesn't exist.")
+module.exports.sendNotFound = (req, res, data) => {
+    res.status(httpCodes.notFound).send(data || "The requested resource doesn't exist.")
+}
+
+module.exports.internalServerError = (req, res, data) => {
+    res.status(httpCodes.internalServerError).send(data || "Internal server error, we are working on the problem.")
 }
