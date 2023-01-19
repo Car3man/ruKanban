e77cf2e083abcdf523300421e145ac27e6e56556
name <email>
2023-01-19 09:33:15 +0300

    ~ updated utils - renamed method to escape single object - added method to escape array

--- app/common/utils.js
+++ app/common/utils.js
@@ -3,10 +3,17 @@
  * @param {Object} object
  * @returns {Object}
  */
-module.exports.escapeBigInt = (object) => {
+module.exports.escapeObjectBigInt = (object) => {
   const escapedObject = object;
   Object.keys(object).forEach((key) => {
     escapedObject[key] = typeof object[key] === 'bigint' ? object[key].toString() : object[key];
   });
   return escapedObject;
 };
+
+/**
+ * Transforms BigInt array into String array
+ * @param {Array<BigInt>} array
+ * @returns {Array<string>}
+ */
+module.exports.escapeArrayBigInt = (array) => array.map((x) => x.toString());
